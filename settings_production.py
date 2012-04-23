3ed426f0782d1174564935d75a4bdfa9086253c9
Dario Alvarez <dario.alvarezmiranda@wsu.edu>
2012-04-23 13:34:51 -0700

    Splitted settings into development and production versions

---
+++ settings_production.py
@@ -0,0 +1,19 @@
+#Settings specific for production environment
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/var/www/html/django/library_value/db/sdw.s3db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/var/www/html/django/library_value/templates',
+)
